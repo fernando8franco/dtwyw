ce31c1ba75a5b00905364ced3ff1f653d5c8cf7e
fernando8franco <francoduranluisfernando@gmail.com>
2025-10-27 19:17:09 -0600

    generateConfigPdfsFile function added

--- cmd/dtwyw/handler_compress.go
+++ cmd/dtwyw/handler_compress.go
@@ -1,8 +1,8 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
-	"fmt"
 	"os"
 	"path/filepath"
 	"strings"
@@ -16,8 +16,17 @@ const (
 	compressPdfsDir = "compress_pdfs"
 )
 
+type PDFsConfig struct {
+	Path    string `json:"path"`
+	Name    string `json:"name"`
+	NewName string `json:"new_name"`
+	Title   string `json:"title"`
+	Author  string `json:"author"`
+}
+
 func HandlerCompress(s *state, cmd command) error {
-	pdfs, err := getPDFs()
+	generateConfigPdfsFile()
+	/* pdfs, err := getPDFs()
 	if err != nil {
 		return err
 	}
@@ -25,9 +34,18 @@ func HandlerCompress(s *state, cmd command) error {
 	for filename, path := range pdfs {
 		fmt.Println(filename + " " + path)
 
-		startResponse, err := callWithRetry(s, func(t string) (api.StartResponse, error) {
-			return api.Start(t)
-		})
+		keyInfo := s.cfg.GetKeyInfo()
+		key := keyInfo.Key
+		token := keyInfo.Token
+
+		startResponse, err := callWithRetry(
+			s,
+			key,
+			&token,
+			func(t string) (api.StartResponse, error) {
+				return api.Start(t)
+			},
+		)
 		if err != nil {
 			return err
 		}
@@ -36,9 +54,14 @@ func HandlerCompress(s *state, cmd command) error {
 		server := startResponse.Server
 		task := startResponse.Task
 
-		uploadResponse, err := callWithRetry(s, func(t string) (api.UploadResponse, error) {
-			return api.Upload(server, task, path, t)
-		})
+		uploadResponse, err := callWithRetry(
+			s,
+			key,
+			&token,
+			func(t string) (api.UploadResponse, error) {
+				return api.Upload(server, task, path, t)
+			},
+		)
 		if err != nil {
 			return err
 		}
@@ -46,9 +69,14 @@ func HandlerCompress(s *state, cmd command) error {
 		fmt.Println(uploadResponse)
 		serverFilename := uploadResponse.ServerFilename
 
-		processResponse, err := callWithRetry(s, func(t string) (api.ProcessResponse, error) {
-			return api.Process(server, task, serverFilename, filename, "test", "UAEH", t)
-		})
+		processResponse, err := callWithRetry(
+			s,
+			key,
+			&token,
+			func(t string) (api.ProcessResponse, error) {
+				return api.Process(server, task, serverFilename, filename, "test", "UAEH", t)
+			},
+		)
 		if err != nil {
 			return err
 		}
@@ -56,24 +84,26 @@ func HandlerCompress(s *state, cmd command) error {
 		fmt.Println(processResponse)
 		compressPdfsPath := strings.Replace(path, pdfsDir, compressPdfsDir, 1)
 
-		dowloadResponse, err := callWithRetry(s, func(t string) (api.DowloadResponse, error) {
-			return api.Dowload(server, task, compressPdfsPath, t)
-		})
+		dowloadResponse, err := callWithRetry(
+			s,
+			key,
+			&token,
+			func(t string) (api.DowloadResponse, error) {
+				return api.Dowload(server, task, compressPdfsPath, t)
+			},
+		)
 		if err != nil {
 			return err
 		}
+
 		fmt.Println(dowloadResponse)
-	}
+	} */
 
 	return nil
 }
 
-func callWithRetry[T any](s *state, apiFunc func(t string) (T, error)) (T, error) {
-	keyInfo := s.cfg.GetKeyInfo()
-	key := keyInfo.Key
-	token := keyInfo.Token
-
-	response, err := apiFunc(token)
+func callWithRetry[T any](s *state, key string, token *string, apiFunc func(t string) (T, error)) (T, error) {
+	response, err := apiFunc(*token)
 
 	if errors.Is(err, api.ErrUnauthorized) {
 		newToken, errToken := api.GetToken(key)
@@ -88,12 +118,49 @@ func callWithRetry[T any](s *state, apiFunc func(t string) (T, error)) (T, error
 			return zero, errToken
 		}
 
-		token = newToken
+		*token = newToken
 
-		response, err = apiFunc(token)
+		response, err = apiFunc(*token)
 	}
 
 	return response, err
+}
+
+func generateConfigPdfsFile() error {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return err
+	}
+
+	configPdfsPath := filepath.Join(homeDir, dtwywDir, pdfsDir, "config.pdfs.json")
+	configPdfsFile, err := os.Create(configPdfsPath)
+	if err != nil {
+		return err
+	}
+	defer configPdfsFile.Close()
+
+	pdfs, err := getPDFs()
+	if err != nil {
+		return err
+	}
+
+	pdfsInfo := []PDFsConfig{}
+	for filename, path := range pdfs {
+		newPDF := PDFsConfig{
+			Name: filename,
+			Path: path,
+		}
+		pdfsInfo = append(pdfsInfo, newPDF)
+	}
+
+	encoder := json.NewEncoder(configPdfsFile)
+	encoder.SetIndent("", "  ")
+
+	if err := encoder.Encode(pdfsInfo); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func getPDFs() (pdfs map[string]string, err error) {
@@ -116,18 +183,4 @@ func getPDFs() (pdfs map[string]string, err error) {
 	}
 
 	return pdfs, err
-}
-
-func getToken(s *state, key string) (token string, err error) {
-	token, err = api.GetToken(key)
-	if err != nil {
-		return "", err
-	}
-
-	err = s.cfg.SetToken(key, token)
-	if err != nil {
-		return "", err
-	}
-
-	return token, nil
 }
